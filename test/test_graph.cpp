5217954d045f82c3e9789dfbf9bb870a1e131727
UziaPeer <uzia.Peer@msmail.ariel.ac.il>
2025-04-07 19:40:35 +0300

    Update test_graph.cpp

--- test/test_graph.cpp
+++ test/test_graph.cpp
@@ -7,32 +7,83 @@
 
 using namespace graph;
 
-TEST_CASE("Graph basic operations") {
+TEST_CASE("Graph - addEdge and removeEdge") {
     Graph g(3);
     g.addEdge(0, 1);
     g.addEdge(1, 2);
+
+    // הקשת קיימת
+    CHECK(g.getAdjList(0) != nullptr);
+    CHECK(g.getAdjList(1) != nullptr);
+
+    // הסרת קשת קיימת
+    g.removeEdge(0, 1);
+    Node* curr = g.getAdjList(0);
+    while (curr) {
+        CHECK(curr->vertex != 1);
+        curr = curr->next;
+    }
+
+    // הסרת קשת שאינה קיימת אמורה לזרוק חריגה
     CHECK_THROWS(g.removeEdge(0, 2));
-    g.removeEdge(1, 2);
-    g.addEdge(0, 2);
-    g.print_graph(); // מדפיס את הגרף לבדיקה ידנית
 }
 
-TEST_CASE("BFS and DFS trees") {
-    Graph g(4);
+TEST_CASE("BFS algorithm") {
+    Graph g(5);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 3);
+    g.addEdge(2, 4);
+
     Graph bfs_tree = Algorithms::bfs(g, 0);
-    Graph dfs_tree = Algorithms::dfs(g, 0);
-    CHECK(bfs_tree.getNumVertices() == 4);
-    CHECK(dfs_tree.getNumVertices() == 4);
+    CHECK(bfs_tree.getNumVertices() == 5);
+    CHECK(bfs_tree.getAdjList(1) != nullptr);
+    CHECK(bfs_tree.getAdjList(2) != nullptr);
 }
 
-TEST_CASE("Dijkstra on weighted graph") {
-    Graph g(3);
+TEST_CASE("DFS algorithm") {
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+
+    Graph dfs_tree = Algorithms::dfs(g, 0);
+    CHECK(dfs_tree.getNumVertices() == 5);
+}
+
+TEST_CASE("Dijkstra algorithm") {
+    Graph g(4);
     g.addEdge(0, 1, 1);
     g.addEdge(1, 2, 2);
     g.addEdge(0, 2, 4);
-    Graph d = Algorithms::dijkstra(g, 0);
-    CHECK(d.getNumVertices() == 3);
+    g.addEdge(2, 3, 1);
+
+    Graph dijkstra_tree = Algorithms::dijkstra(g, 0);
+    CHECK(dijkstra_tree.getNumVertices() == 4);
+    CHECK(dijkstra_tree.getAdjList(3) != nullptr);
 }
+
+TEST_CASE("Prim algorithm") {
+    Graph g(4);
+    g.addEdge(0, 1, 2);
+    g.addEdge(1, 2, 3);
+    g.addEdge(2, 3, 1);
+    g.addEdge(3, 0, 4);
+
+    Graph mst = Algorithms::prim(g);
+    CHECK(mst.getNumVertices() == 4);
+}
+
+TEST_CASE("Kruskal algorithm") {
+    Graph g(4);
+    g.addEdge(0, 1, 10);
+    g.addEdge(0, 2, 6);
+    g.addEdge(0, 3, 5);
+    g.addEdge(1, 3, 15);
+    g.addEdge(2, 3, 4);
+
+    Graph mst = Algorithms::kruskal(g);
+    CHECK(mst.getNumVertices() == 4);
+}
+
