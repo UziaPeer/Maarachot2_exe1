fd9c707f3076bc1a4ca95ae9e8df137aebd0a7e4
UziaPeer <uzia.Peer@msmail.ariel.ac.il>
2025-04-07 22:07:39 +0300

    Update Graph.hpp

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -5,27 +5,32 @@
 
 namespace graph {
 
+// מבנה של צומת ברשימת שכנויות (קודקוד יעד, משקל, מצביע לצומת הבא)
 struct Node {
     int vertex;
     int weight;
     Node* next;
 };
 
+// מחלקת גרף לא מכוונת עם משקלים, לפי רשימת שכנויות
 class Graph {
 private:
     int numVertices;
     Node** adjList;
 
 public:
+ // בונה גרף עם מספר קודקודים נתון (ללא קשתות)
     Graph(int vertices);
+// הורס את כל הזיכרון שהוקצה לרשימות
     ~Graph();
     void addEdge(int src, int dest, int weight = 1);
     void removeEdge(int src, int dest);
     void print_graph() const;
     int getNumVertices() const;
+// מחזיר את תחילת רשימת השכנים של קודקוד מסוים
     Node* getAdjList(int vertex) const;
 };
 
-} // namespace graph
+} 
 
 #endif // GRAPH_HPP
