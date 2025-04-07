6c6ce296ec744c4111bf5c5cfeec8ec5dad9f51e
UziaPeer <uzia.Peer@msmail.ariel.ac.il>
2025-04-07 17:24:00 +0300

    Create Algorithms.hpp

---
+++ include/Algorithms.hpp
@@ -0,0 +1,21 @@
+// include/Algorithms.hpp
+// Email: peeruzia@gmail.com
+#ifndef ALGORITHMS_HPP
+#define ALGORITHMS_HPP
+
+#include "Graph.hpp"
+
+namespace graph {
+
+class Algorithms {
+public:
+    static Graph bfs(const Graph& g, int start);
+    static Graph dfs(const Graph& g, int start);
+    static Graph dijkstra(const Graph& g, int start);
+    static Graph prim(const Graph& g);
+    static Graph kruskal(const Graph& g);
+};
+
+} // namespace graph
+
+#endif // ALGORITHMS_HPP
