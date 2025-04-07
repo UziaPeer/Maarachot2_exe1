111cd7280d9e49704267fdfc98df7fbaccdb4dbd
UziaPeer <uzia.Peer@msmail.ariel.ac.il>
2025-04-07 23:24:12 +0300

    Update test_graph.cpp

--- test/test_graph.cpp
+++ test/test_graph.cpp
@@ -1,5 +1,6 @@
 // test/test_graph.cpp
 // Email: peeruzia@gmail.com
+// קובץ הבדיקות הראשי - כולל את הספרייה doctest
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "Graph.hpp"
@@ -7,16 +8,17 @@
 
 using namespace graph;
 
+// בדיקת הוספה והסרה של קשתות בגרף
 TEST_CASE("Graph - addEdge and removeEdge") {
     Graph g(3);
     g.addEdge(0, 1);
     g.addEdge(1, 2);
 
-    // הקשת קיימת
+    // בודקים שהקשתות נוספו
     CHECK(g.getAdjList(0) != nullptr);
     CHECK(g.getAdjList(1) != nullptr);
 
-    // הסרת קשת קיימת
+    // מסירים קשת ובודקים שהיא לא קיימת יותר
     g.removeEdge(0, 1);
     Node* curr = g.getAdjList(0);
     while (curr) {
@@ -24,10 +26,11 @@ TEST_CASE("Graph - addEdge and removeEdge") {
         curr = curr->next;
     }
 
-    // הסרת קשת שאינה קיימת אמורה לזרוק חריגה
+    // מנסים להסיר קשת שלא קיימת - אמור לזרוק חריגה
     CHECK_THROWS(g.removeEdge(0, 2));
 }
 
+// בדיקת BFS - האם האלגוריתם יוצר עץ תקין
 TEST_CASE("BFS algorithm") {
     Graph g(5);
     g.addEdge(0, 1);
@@ -36,11 +39,14 @@ TEST_CASE("BFS algorithm") {
     g.addEdge(2, 4);
 
     Graph bfs_tree = Algorithms::bfs(g, 0);
+
+    // עץ צריך לכלול את כל הצמתים
     CHECK(bfs_tree.getNumVertices() == 5);
     CHECK(bfs_tree.getAdjList(1) != nullptr);
     CHECK(bfs_tree.getAdjList(2) != nullptr);
 }
 
+// בדיקת DFS - עץ סריקה מעומק
 TEST_CASE("DFS algorithm") {
     Graph g(5);
     g.addEdge(0, 1);
@@ -52,6 +58,7 @@ TEST_CASE("DFS algorithm") {
     CHECK(dfs_tree.getNumVertices() == 5);
 }
 
+// בדיקת Dijkstra - האם האלגוריתם מוצא מסלולים קצרים
 TEST_CASE("Dijkstra algorithm") {
     Graph g(4);
     g.addEdge(0, 1, 1);
@@ -60,10 +67,12 @@ TEST_CASE("Dijkstra algorithm") {
     g.addEdge(2, 3, 1);
 
     Graph dijkstra_tree = Algorithms::dijkstra(g, 0);
+
     CHECK(dijkstra_tree.getNumVertices() == 4);
-    CHECK(dijkstra_tree.getAdjList(3) != nullptr);
+    CHECK(dijkstra_tree.getAdjList(3) != nullptr); // מסלול אמור להגיע ל-3
 }
 
+// בדיקת Prim - בניית MST
 TEST_CASE("Prim algorithm") {
     Graph g(4);
     g.addEdge(0, 1, 2);
@@ -75,6 +84,7 @@ TEST_CASE("Prim algorithm") {
     CHECK(mst.getNumVertices() == 4);
 }
 
+// בדיקת Kruskal - גם הוא בונה MST
 TEST_CASE("Kruskal algorithm") {
     Graph g(4);
     g.addEdge(0, 1, 10);
